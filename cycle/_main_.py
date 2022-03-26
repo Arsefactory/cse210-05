fc60bdd2d1d830c46065483df7182b2c6d9bd511
Arsefactory <sajnin5@gmail.com>
2022-03-26 13:43:19 -0600

    Added more classes and changed Main

---
+++ cycle/_main_.py
@@ -0,0 +1,22 @@
+# Names of People in Group: 
+# Dawson Packer
+# John Pischke
+# Logan Crossley
+
+FRAME_RATE = 12
+MAX_X = 900
+MAX_Y = 600
+CELL_SIZE = 15
+FONT_SIZE = 15
+COLS = 60
+ROWS = 40
+CAPTION = 'Cycle'
+
+
+def main():
+    # start the game
+    pass
+
+
+if __name__ == '__main__':
+    main()
